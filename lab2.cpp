4d527233db760ed7f8f1d57bf2fc0168ce499e36
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2020-11-10 19:11:15 +0000

    .

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,109 @@
 #include <iostream>
+using namespace std;
+
+class wektor
+{
+public:
+    wektor(int n)
+    {
+        tab = new double[n];
+        for (int i = 0; i < n; i++) {
+            tab[i] = 0.0;
+        }
+        dlugosc   = n;
+        pojemnosc = n;
+    };
+    ~wektor()
+    {
+        delete[] tab;
+        cout << "i cyk nie ma tablicy" << endl;
+    };
+    void zmienDlugosc(int d)
+    {
+        if (d <= dlugosc) {
+            for (int i = d; i < dlugosc; i++) {
+                tab[i] = 0.0;
+            }
+            dlugosc = d;
+        }
+        else {
+            double* tab2 = tab;
+            tab          = new double[d];
+            for (int i = 0; i < dlugosc; i++) {
+                tab[i] = tab2[i];
+            }
+            for (int i = dlugosc; i < d; i++) {
+                tab[i] = 0.0;
+            }
+            pojemnosc = d;
+            dlugosc   = d;
+            delete[] tab2;
+        }
+    };
+
+    void getterDlugosc() { cout << "dlugosc rowna sie= " << dlugosc << endl; }
+    void getterPojemnosc() { cout << "pojemnosc rowna sie= " << pojemnosc << endl; }
+    void print()
+    {
+        cout << "kolejne argumenty wektora" << endl;
+        for (int i = 0; i < dlugosc; i++) {
+            cout << tab[i] << endl;
+        };
+    };
+    double& operator[](int index)
+    {
+        if (index >= dlugosc) {
+            zmienDlugosc(index);
+        }
+        return tab[index];
+    }
+
+private:
+    double* tab;
+    int     dlugosc;
+    int     pojemnosc;
+};
+struct Kokardka
+{
+    Kokardka() { dlugosc = 42; }
+    Kokardka(int d) { dlugosc = d; }
+
+    int dlugosc;
+};
+
+struct Prezent
+{
+    Prezent(int dk)
+    {
+        cout << k.dlugosc << endl;
+
+        k.dlugosc = dk;
+        cout << k.dlugosc << endl;
+    }
+
+    Kokardka k;
+    // Inne pola ...
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Prezent p1(1);
+
+    /* wektor f1(5);
+     cout << "czy to juz? " << endl;
+     f1.getterDlugosc();
+     f1.getterPojemnosc();
+     f1.print();
+     f1.zmienDlugosc(3);
+     f1.print();
+     f1.getterDlugosc();
+     f1.getterPojemnosc();
+     cout << "czy to juz? " << endl;
+     f1.print();
+     f1.zmienDlugosc(8);
+     f1.getterDlugosc();
+     f1.getterPojemnosc();
+     f1.print();
+     cout << "czy to juz? " << endl;
+     */
 }
--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,5 +4,43 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+  public:
+    Resource* res;
+
+    ResourceManager()
+    {
+      res= new Resource;
+    };
+
+    ResourceManager(const ResourceManager& resM) { res = new Resource(*(resM.res)); }
+
+    ResourceManager& operator=(const ResourceManager&  resM)
+    {
+      if (this != &resM)
+       {
+             *R = *(resM.res);
+       }
+       return *this;
+
+    }
+    ResourceManager(ResourceManager&& resM) : res(resM.res)
+    {
+        resM.res         = nullptr;
+    }
+    ResourceManager& operator=(ResourceManager&& reM )
+    {
+        if (&resM == this)
+        {
+            return *this;
+            delete res;
+            res=resM.res;
+            resM.res = nullptr;
+            return *this;
+        }
+    }
+    ~ResourceManager()
+    {
+        delete res;
+    }
+    double get() { return Rres->get(); };  
 };
